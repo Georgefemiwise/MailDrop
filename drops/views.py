6151b3222c0f66ad6ecd8f9a08868c870ede7849
georgefemiwise <georgewisw1010@gmail.com>
2022-11-27 10:44:16 +0000

    email geneator developed to store in email

--- drops/views.py
+++ drops/views.py
@@ -4,37 +4,27 @@ from schoolMailGenerator.generateEmailAddress import GenerateEmail
 
 # Create your views here.
 
-def emailer(program,year):
-     return f"{program}{year}@ttu.edu.gh"
-
-
-
 
 def home(request):
+     class DropAddress(GenerateEmail):
+          def __init__(self, program:str, year:int, numInClass:int):
+               GenerateEmail.__init__(self, program, year)
+               self.numInClass = numInClass
+
+          def genAddress(self):
+               for num in range(1,int(self.numInClass)+1):
+                    generating = f"{self.program}/{str(self.year)}{num}@ttu.edu.gh"
+                    address =Addresses.objects.create(Address=generating)
+                    address.save()
  
      if request.method == 'POST':
           number = request.POST['number']
           program = request.POST['program']
           year = request.POST['year']
 
-          @GenerateEmail()
+          address =DropAddress(numInClass=number,program=program,year=year)
+          address.genAddress()
 
-
-
-
-               # p?rogram=program, year=int(year)
-          def classNumber(number):
-               for num in range(1,number+1):
-                    generating = f"{self.program}/{str(self.year)}/{num}@ttu.edu.gh"
-               return generating
-
-
-         
-          address =Addresses.objects(classNumber())
-          address.save()
-
-          
-         
 
 
 
