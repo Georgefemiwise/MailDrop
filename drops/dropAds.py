6732448758807852835df138d2627c795bb90db2
georgefemiwise <georgewise1010@gmail.com>
2023-03-09 13:57:17 +0000

    modify

--- drops/dropAds.py
+++ drops/dropAds.py
@@ -22,8 +22,25 @@ class DropAddress(EmailAdsBoiler):
                     address = BDA.objects.create(Address=generating)
                     address.save() # Add parentheses to save the new record to the database
           
-# Use the class to generate and print 5 email addresses
-for i in range(5):
-     address_gen = DropAddress("bcict", 21, 5)
-     address_gen.genAddress()
-     print(f"bcict{str(21)}{i:02}@ttu.edu.gh")
+
+# ============================================================================================================
+
+# Import necessary modules and classes
+from schoolMailGenerator.AddressBoiler import EmailAdsBoiler
+from .models import BachelorsDegreeAddresses as BDA
+
+# Define a class to generate and save email addresses
+class DropAddress(EmailAdsBoiler):
+     def __init__(self, program:str, year:int, numInClass:int):
+          super().__init__(program, year)
+          self.numInClass = numInClass
+
+     def genAddress(self):
+          # Generate and save email addresses for Bachelors degree students
+          for num in range(1,int(self.numInClass)+1):
+               generating = f"{self.program}{str(self.year)}{num:03}@ttu.edu.gh"
+               
+               if not BDA.objects.filter(Address=generating).exists(): 
+                    # If the email address does not already exist, create a new record
+                    address = BDA.objects.create(Address=generating)
+                    address.save() # Add parentheses to save the new record to the database
---
+++ sender.py
@@ -0,0 +1,24 @@
+import smtplib
+import datetime
+import time
+from django.core.mail import EmailMessage
+
+# Set up SMTP connection
+smtp_server = 'smtp.gmail.com'
+smtp_port = 587
+sender_email = 'sender@gmail.com'
+sender_password = 'sender_password'
+recipient_email = 'recipient@example.com'
+
+# Send message every Saturday
+while True:
+    today = datetime.datetime.today()
+    if today.weekday() == 5: # Saturday
+        subject = 'Weekly update'
+        body = 'Hello,\n\nThis is your weekly update.\n\nBest regards,\nSender'
+        email = EmailMessage(subject, body, sender_email, [recipient_email])
+        email.send(fail_silently=False)
+        print(f'Message sent to {recipient_email} on {today}.')
+        time.sleep(86400) # wait for 24 hours
+    else:
+        time.sleep(3600) # wait for 1 hour
